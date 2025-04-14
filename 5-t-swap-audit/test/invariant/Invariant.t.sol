6a951ac6376a2ca25920617d9cf269430df6483b
s3bc40 <s3bc40@gmail.com>
2025-04-14 22:19:21 +0200

    feat: invariant tests tswap and prepare handler
    - added invariant tests for tswap and prepare handler
    - added mock erc20 token for testing
    
    Handler test better for testing tswap since we can init variables without messing with the contracts

--- 5-t-swap-audit/test/invariant/Invariant.t.sol
+++ 5-t-swap-audit/test/invariant/Invariant.t.sol
@@ -2,3 +2,49 @@
 pragma solidity 0.8.20;
 
 import {Test} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {ERC20Mock} from "../mocks/ERC20Mock.sol";
+import {PoolFactory} from "../../src/PoolFactory.sol";
+import {TSwapPool} from "../../src/TSwapPool.sol";
+
+int256 constant STARTING_X = 100e18; // Starting pool token balance
+int256 constant STARTING_Y = 50e18; // Starting WETH balance
+
+contract Invariant is StdInvariant, Test {
+    // these pools have two assets
+    ERC20Mock poolToken;
+    ERC20Mock weth;
+
+    // we need the contracts
+    PoolFactory factory;
+    TSwapPool pool; // poolToken / WETH
+
+    // only one pool for the invariant test for now
+
+    function setUp() public {
+        weth = new ERC20Mock();
+        poolToken = new ERC20Mock();
+        factory = new PoolFactory(address(weth));
+        pool = TSwapPool(factory.createPool(address(poolToken)));
+
+        //create those initial x & y balances
+        poolToken.mint(address(this), uint256(STARTING_X));
+        weth.mint(address(this), uint256(STARTING_Y));
+
+        poolToken.approve(address(pool), type(uint256).max);
+        weth.approve(address(pool), type(uint256).max);
+
+        // Deposit to the pools
+        pool.deposit(
+            uint256(STARTING_Y),
+            uint256(STARTING_Y),
+            uint256(STARTING_X),
+            uint64(block.timestamp)
+        );
+    }
+
+    function statefulFuzz_testConstantProductFormulasStaysTheSame() public {
+        // the change in the pool size of WETH should follow
+        // Handler better to use to avoid messing with the contracts!
+    }
+}
